a422df32ef85479eab19c1630f942188267b426f
menggerSherry <1030100815@qq.com>
2022-12-07 15:54:25 +0800

    updata requirements

--- evaluate_plus.py
+++ evaluate_plus.py
@@ -5,213 +5,57 @@
 import numpy as np
 import pandas as pd
 import click as ck
-from sklearn.metrics import classification_report
-from sklearn.metrics.pairwise import cosine_similarity
 import sys
 from collections import deque
 import time
 import logging
-from sklearn.metrics import roc_curve, auc, matthews_corrcoef
-from scipy.spatial import distance
-from scipy import sparse
 import math
-#from utils2 import FUNC_DICT, Ontology, NAMESPACES
+from script.utils import FUNC_DICT, Ontology, NAMESPACES
 from matplotlib import pyplot as plt
+import json
 
-#copied from utils.py
-from collections import deque, Counter
-import warnings
-#import pandas as pd
-#import numpy as np
-#from xml.etree import ElementTree as ET
-#import math
-
-BIOLOGICAL_PROCESS = 'GO:0008150'
-MOLECULAR_FUNCTION = 'GO:0003674'
-CELLULAR_COMPONENT = 'GO:0005575'
-FUNC_DICT = {
-    'cc': CELLULAR_COMPONENT,
-    'mf': MOLECULAR_FUNCTION,
-    'bp': BIOLOGICAL_PROCESS}
-
-NAMESPACES = {
-    'cc': 'cellular_component',
-    'mf': 'molecular_function',
-    'bp': 'biological_process'
-}
-
-EXP_CODES = set([
-    'EXP', 'IDA', 'IPI', 'IMP', 'IGI', 'IEP', 'TAS', 'IC',])
-#    'HTP', 'HDA', 'HMP', 'HGI', 'HEP'])
-CAFA_TARGETS = set([
-    '10090', '223283', '273057', '559292', '85962',
-    '10116',  '224308', '284812', '7227', '9606',
-    '160488', '237561', '321314', '7955', '99287',
-    '170187', '243232', '3702', '83333', '208963',
-    '243273', '44689', '8355'])
-
-def is_cafa_target(org):
-    return org in CAFA_TARGETS
-
-def is_exp_code(code):
-    return code in EXP_CODES
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
 
-class Ontology(object):
-
-    def __init__(self, filename='data/go.obo', with_rels=False):
-        self.ont = self.load(filename, with_rels)
-        self.ic = None
-
-    def has_term(self, term_id):
-        return term_id in self.ont
-
-    def calculate_ic(self, annots):
-        cnt = Counter()
-        for x in annots:
-            cnt.update(x)
-        self.ic = {}
-        for go_id, n in cnt.items():
-            parents = self.get_parents(go_id)
-            if len(parents) == 0:
-                min_n = n
-            else:
-                min_n = min([cnt[x] for x in parents])
-            self.ic[go_id] = math.log(min_n / n, 2)
+@ck.command()
+@ck.option(
+    '--train-data-file', '-trdf', default='data/train_data.pkl',
+    help='Data file with training features')
+@ck.option(
+    '--test-data-file', '-tsdf', default='data/predictions.pkl',
+    help='Test data file')
+@ck.option(
+    '--terms-file', '-tf', default='data/terms.pkl',
+    help='Data file with sequences and complete set of annotations')
+@ck.option(
+    '--go-file', '-gf', default='data/go.obo',
+    help='Test data file')    
+@ck.option(
+    '--diamond-scores-file', '-dsf', default='data/test_diamond.res',
+    help='Diamond output')
+@ck.option(
+    '--ont', '-o', default='mf',
+    help='GO subontology (bp, mf, cc)')
+@ck.option(
+    '--alpha', '-a', default=50,
+    help='Alpha for for combining scores')
     
-    def get_ic(self, go_id):
-        if self.ic is None:
-            raise Exception('Not yet calculated')
-        if go_id not in self.ic:
-            return 0.0
-        return self.ic[go_id]
 
-    def load(self, filename, with_rels):
-        ont = dict()
-        obj = None
-        with open(filename, 'r') as f:
-            for line in f:
-                line = line.strip()
-                if not line:
-                    continue
-                if line == '[Term]':
-                    if obj is not None:
-                        ont[obj['id']] = obj
-                    obj = dict()
-                    obj['is_a'] = list()
-                    obj['part_of'] = list()
-                    obj['regulates'] = list()
-                    obj['alt_ids'] = list()
-                    obj['is_obsolete'] = False
-                    continue
-                elif line == '[Typedef]':
-                    obj = None
-                else:
-                    if obj is None:
-                        continue
-                    l = line.split(": ")
-                    if l[0] == 'id':
-                        obj['id'] = l[1]
-                    elif l[0] == 'alt_id':
-                        obj['alt_ids'].append(l[1])
-                    elif l[0] == 'namespace':
-                        obj['namespace'] = l[1]
-                    elif l[0] == 'is_a':
-                        obj['is_a'].append(l[1].split(' ! ')[0])
-                    elif with_rels and l[0] == 'relationship':
-                        it = l[1].split()
-                        # add all types of relationships
-                        obj['is_a'].append(it[1])
-                    elif l[0] == 'name':
-                        obj['name'] = l[1]
-                    elif l[0] == 'is_obsolete' and l[1] == 'true':
-                        obj['is_obsolete'] = True
-        if obj is not None:
-            ont[obj['id']] = obj
-        for term_id in list(ont.keys()):
-            for t_id in ont[term_id]['alt_ids']:
-                ont[t_id] = ont[term_id]
-            if ont[term_id]['is_obsolete']:
-                del ont[term_id]
-        for term_id, val in ont.items():
-            if 'children' not in val:
-                val['children'] = set()
-            for p_id in val['is_a']:
-                if p_id in ont:
-                    if 'children' not in ont[p_id]:
-                        ont[p_id]['children'] = set()
-                    ont[p_id]['children'].add(term_id)
-        return ont
+def main(train_data_file, test_data_file, terms_file, go_file,
+         diamond_scores_file, ont, alpha):
 
-
-    def get_anchestors(self, term_id):
-        if term_id not in self.ont:
-            return set()
-        term_set = set()
-        q = deque()
-        q.append(term_id)
-        while(len(q) > 0):
-            t_id = q.popleft()
-            if t_id not in term_set:
-                term_set.add(t_id)
-                for parent_id in self.ont[t_id]['is_a']:
-                    if parent_id in self.ont:
-                        q.append(parent_id)
-        return term_set
-
-
-    def get_parents(self, term_id):
-        if term_id not in self.ont:
-            return set()
-        term_set = set()
-        for parent_id in self.ont[term_id]['is_a']:
-            if parent_id in self.ont:
-                term_set.add(parent_id)
-        return term_set
-
-
-    def get_namespace_terms(self, namespace):
-        terms = set()
-        for go_id, obj in self.ont.items():
-            if obj['namespace'] == namespace:
-                terms.add(go_id)
-        return terms
-
-    def get_namespace(self, term_id):
-        return self.ont[term_id]['namespace']
-    
-    def get_term_set(self, term_id):
-        if term_id not in self.ont:
-            return set()
-        term_set = set()
-        q = deque()
-        q.append(term_id)
-        while len(q) > 0:
-            t_id = q.popleft()
-            if t_id not in term_set:
-                term_set.add(t_id)
-                for ch_id in self.ont[t_id]['children']:
-                    q.append(ch_id)
-        return term_set
-
-
-
-def evaluate_deepgoplus(train_data_file, test_data_file, terms_file,
-         diamond_scores_file, gofile, ont, preds=None, export=False,evaluate=True,verbose=False):
-
-    go_rels = Ontology(gofile, with_rels=True)
-    if(isinstance(terms_file,list) or isinstance(terms_file,np.ndarray)):
-        terms = terms_file
-    else:
-        terms_df = pd.read_pickle(terms_file)
-        terms = terms_df['terms'].values.flatten()
+    go_rels = Ontology(go_file, with_rels=True)
+    terms_df = pd.read_pickle(terms_file)
+    terms = terms_df['terms'].values.flatten()
     terms_dict = {v: i for i, v in enumerate(terms)}
 
     train_df = pd.read_pickle(train_data_file)
     test_df = pd.read_pickle(test_data_file)
-    annotations = train_df['annotations'].values
+    print("Length of test set: " + str(len(test_df)))
+    
+    annotations = train_df['prop_annotations'].values
     annotations = list(map(lambda x: set(x), annotations))
-    test_annotations = test_df['annotations'].values
+    test_annotations = test_df['prop_annotations'].values
     test_annotations = list(map(lambda x: set(x), test_annotations))
     go_rels.calculate_ic(annotations + test_annotations)
 
@@ -235,6 +79,7 @@ def evaluate_deepgoplus(train_data_file, test_data_file, terms_file,
             diamond_scores[it[0]][it[1]] = float(it[2])
 
     blast_preds = []
+    #print('Diamond preds')
     for i, row in enumerate(test_df.itertuples()):
         annots = {}
         prot_id = row.proteins
@@ -262,16 +107,23 @@ def evaluate_deepgoplus(train_data_file, test_data_file, terms_file,
     # DeepGOPlus
     go_set = go_rels.get_namespace_terms(NAMESPACES[ont])
     go_set.remove(FUNC_DICT[ont])
-    labels = test_df['annotations'].values
+    labels = test_df['prop_annotations'].values
     labels = list(map(lambda x: set(filter(lambda y: y in go_set, x)), labels))
     # print(len(go_set))
     deep_preds = []
-    alphas = {NAMESPACES['mf']: 0.54, NAMESPACES['bp']: 0.58, NAMESPACES['cc']: 0.47}
+    # alphas = {NAMESPACES['mf']: 0.54, NAMESPACES['bp']: 0.58, NAMESPACES['cc']: 0.47}
+    alphas = {NAMESPACES['mf']: 0, NAMESPACES['bp']: 0, NAMESPACES['cc']: 0}
+
+    with open(last_release_metadata, 'r') as f:
+        last_release_data = json.load(f)
+        alpha = last_release_data["alphas"][ont]
+        alphas[NAMESPACES[ont]] = alpha
+
     for i, row in enumerate(test_df.itertuples()):
         annots_dict = blast_preds[i].copy()
         for go_id in annots_dict:
             annots_dict[go_id] *= alphas[go_rels.get_namespace(go_id)]
-        for j, score in enumerate(row.preds if preds is None else preds[i]):
+        for j, score in enumerate(row.preds):
             go_id = terms[j]
             score *= 1 - alphas[go_rels.get_namespace(go_id)]
             if go_id in annots_dict:
@@ -279,117 +131,6 @@ def evaluate_deepgoplus(train_data_file, test_data_file, terms_file,
             else:
                 annots_dict[go_id] = score
         deep_preds.append(annots_dict)
-    
-    if(export):
-        export_cafa(test_df,deep_preds,"DeepGOPlus_1_all.txt")
-    if(evaluate):
-        print("Evaluating scores")
-        compute_prmetrics(labels,deep_preds,go_rels,ont=ont,verbose=verbose)
-        #aucs = compute_roc(labels,deep_preds)
-        #print("aucs:",aucs)
-        #print("mean aucs(predicted):",np.mean(aucs))
-        #print("mean aucs(all):",(np.sum(aucs)+(len(test_annotations)-len(aucs))*0.5)/len(test_annotations))
-
-def evaluate(train_data_file, test_data_file, terms_file,
-         gofile, ont, preds=None, propagate_scores=False,export=False,evaluate=True,verbose=False):
-    '''
-    train_data_file: path to train_data.pkl
-    test_data_file: path to test_data.pkl
-    terms_file: path to terms.pkl or just a list or nparray of labels
-    '''
-    go_rels = Ontology(gofile, with_rels=True)
-    
-    if(isinstance(terms_file,list) or isinstance(terms_file,np.ndarray)):
-        terms = terms_file
-    else:
-        terms_df = pd.read_pickle(terms_file)
-        terms = terms_df['terms'].values.flatten()
-    terms_dict = {v: i for i, v in enumerate(terms)}
-
-    train_df = pd.read_pickle(train_data_file)
-    test_df = pd.read_pickle(test_data_file)
-    annotations = train_df['annotations'].values
-    annotations = list(map(lambda x: set(x), annotations))
-    test_annotations = test_df['annotations'].values
-    test_annotations = list(map(lambda x: set(x), test_annotations))
-    go_rels.calculate_ic(annotations + test_annotations)
-
-    # Print IC values of terms
-    ics = {}
-    for term in terms:
-        ics[term] = go_rels.get_ic(term)
-
-    prot_index = {}
-    for i, row in enumerate(train_df.itertuples()):
-        prot_index[row.proteins] = i
-    
-    go_set = go_rels.get_namespace_terms(NAMESPACES[ont])
-    go_set.remove(FUNC_DICT[ont])
-    
-    labels = test_df['annotations'].values
-    labels = list(map(lambda x: set(filter(lambda y: y in go_set, x)), labels))
-
-
-    if(preds is None):
-        deep_preds = []
-        for i, row in enumerate(test_df.itertuples()):
-            annots_dict = {}
-            for j, score in enumerate(row.preds):
-                go_id = terms[j]
-                annots_dict[go_id] = score
-            deep_preds.append(annots_dict)
-    else:
-        deep_preds = [{terms[i]:y for i,y in enumerate(x)} for x in preds]
-            
-    # Propagate scores (a la deepgo)
-    if(propagate_scores):
-        print("Propagating scores a la deepgo")
-        deepgo_preds = []
-        for annots_dict in deep_preds:
-            annots = {}
-            for go_id, score in annots_dict.items():
-                for a_id in go_rels.get_anchestors(go_id):
-                    if a_id in annots:
-                        annots[a_id] = max(annots[a_id], score)
-                    else:
-                        annots[a_id] = score
-            deepgo_preds.append(annots)
-        deep_preds = deepgo_preds
-    
-    if(export):
-        export_cafa(test_df,deep_preds,"AxialGO_1_all.txt")
-        
-    # compute PR metrics
-    if(evaluate):
-        print("Evaluating scores")
-        compute_prmetrics(labels,deep_preds,go_rels,ont=ont,verbose=False)
-        #aucs = compute_roc(np.array(labels),np.array(deep_preds))
-        #print("aucs:",aucs)
-        #print("mean aucs(predicted):",np.mean(aucs))
-        #print("mean aucs(all):",(np.sum(aucs)+(len(test_annotations)-len(aucs))*0.5)/len(test_annotations))
-        
-def export_cafa(test_df,deep_preds,filename='AxialGO_1_all.txt'):#all->9606 for human
-    print("exporting predictions to CAFA submission format")
-    txt_out=[]
-        
-    txt_out.append('AUTHOR AxialGO\n')
-    txt_out.append('MODEL 1\n')
-    txt_out.append('KEYWORDS sequence alignment.\n')
-    for i, row in enumerate(test_df.itertuples()):
-        prot_id = row.proteins
-        for go_id, score in deep_preds[i].items():
-            #print(f'{prot_id}\t{go_id}\t{score:.2f}')
-            score_str = "{0:.2f}".format(score)
-            if(score_str!="0.00"):
-                txt_out.append(str(prot_id)+"\t"+str(go_id)+"\t"+score_str+"\n")
-    txt_out.append('END')
-    with open(filename, 'w') as f:
-        f.writelines(txt_out)
-
-def compute_prmetrics(labels,deep_preds,go_rels,ont="mf",verbose=False):
-    go_set = go_rels.get_namespace_terms(NAMESPACES[ont])
-    go_set.remove(FUNC_DICT[ont])
-    
     fmax = 0.0
     tmax = 0.0
     precisions = []
@@ -397,10 +138,10 @@ def compute_prmetrics(labels,deep_preds,go_rels,ont="mf",verbose=False):
     smin = 1000000.0
     rus = []
     mis = []
-    for t in range(0, 101):
+    for t in range(1, 101): # the range in this loop has influence in the AUPR output
         threshold = t / 100.0
         preds = []
-        for i in range(len(deep_preds)):
+        for i, row in enumerate(test_df.itertuples()):
             annots = set()
             for go_id, score in deep_preds[i].items():
                 if score >= threshold:
@@ -417,17 +158,18 @@ def compute_prmetrics(labels,deep_preds,go_rels,ont="mf",verbose=False):
         fscore, prec, rec, s, ru, mi, fps, fns = evaluate_annotations(go_rels, labels, preds)
         avg_fp = sum(map(lambda x: len(x), fps)) / len(fps)
         avg_ic = sum(map(lambda x: sum(map(lambda go_id: go_rels.get_ic(go_id), x)), fps)) / len(fps)
-        if(verbose):
-            print(f'{avg_fp} {avg_ic}')
+        #print(f'{avg_fp} {avg_ic}')
         precisions.append(prec)
         recalls.append(rec)
-        print(f'Fscore: {fscore}, Precision: {prec}, Recall: {rec} S: {s}, RU: {ru}, MI: {mi} threshold: {threshold}')
+        # print(f'Fscore: {fscore}, Precision: {prec}, Recall: {rec} S: {s}, RU: {ru}, MI: {mi} threshold: {threshold}')
         if fmax < fscore:
             fmax = fscore
             tmax = threshold
         if smin > s:
             smin = s
-    print(f'Fmax: {fmax:0.3f}, Smin: {smin:0.3f}, threshold: {tmax}')
+    print(f'threshold: {tmax}')
+    print(f'Smin: {smin:0.3f}')
+    print(f'Fmax: {fmax:0.3f}')
     precisions = np.array(precisions)
     recalls = np.array(recalls)
     sorted_index = np.argsort(recalls)
@@ -445,22 +187,9 @@ def compute_prmetrics(labels,deep_preds,go_rels,ont="mf",verbose=False):
     plt.ylabel('Precision')
     plt.title('Area Under the Precision-Recall curve')
     plt.legend(loc="lower right")
-    plt.savefig(f'aupr_{ont}.pdf')
+    plt.savefig(f'results/aupr_{ont}_{alpha:0.2f}.pdf')
     df = pd.DataFrame({'precisions': precisions, 'recalls': recalls})
-    df.to_pickle(f'PR_{ont}.pkl')
-    
-    
-
-def compute_roc(labels, preds):
-    # Compute ROC curve and ROC area for each class
-    fpr, tpr, _ = roc_curve(labels.flatten(), preds.flatten())
-    roc_auc = auc(fpr, tpr)
-    return roc_auc
-
-def compute_mcc(labels, preds):
-    # Compute ROC curve and ROC area for each class
-    mcc = matthews_corrcoef(labels.flatten(), preds.flatten())
-    return mcc
+    df.to_pickle(f'results/PR_{ont}_{alpha:0.2f}.pkl')
 
 def evaluate_annotations(go, real_annots, pred_annots):
     total = 0
@@ -505,5 +234,5 @@ def evaluate_annotations(go, real_annots, pred_annots):
     return f, p, r, s, ru, mi, fps, fns
 
 
-#if __name__ == '__main__':
-#    main()
+if __name__ == '__main__':
+    main()
